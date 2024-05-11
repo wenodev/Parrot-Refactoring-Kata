4ebc389e109bdbca80f3e6f2e0e3271ef6a2cd86
sonjuwon <logicalstark1105@gmail.com>
2024-05-11 17:29:46 +0900

    .

--- Java/src/main/java/parrot/ParrotTypeEnum.java
+++ Java/src/main/java/parrot/ParrotTypeEnum.java
@@ -11,7 +11,6 @@ public enum ParrotTypeEnum {
             case EUROPEAN -> new EuropeanParrot();
             case AFRICAN -> new AfricanParrot(numberOfCoconuts);
             case NORWEGIAN_BLUE -> new NorwegianParrot(voltage, isNailed);
-            default -> throw new IllegalArgumentException("Can not arrange");
         };
     }
 }
