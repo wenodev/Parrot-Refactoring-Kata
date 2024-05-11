1807aac9fd4ff2fb274b7940bea874c0d08d9ae6
sonjuwon <logicalstark1105@gmail.com>
2024-05-11 17:18:05 +0900

    .

--- Java/src/main/java/parrot/EuropeanParrot.java
+++ Java/src/main/java/parrot/EuropeanParrot.java
@@ -1,16 +1,14 @@
 package parrot;
 
+import static parrot.ParrotTypeEnum.EUROPEAN;
+
 public class EuropeanParrot extends Parrot {
     public EuropeanParrot(final int numberOfCoconuts, final double voltage, final boolean isNailed) {
-        super(ParrotTypeEnum.EUROPEAN, numberOfCoconuts, voltage, isNailed);
+        super(EUROPEAN, numberOfCoconuts, voltage, isNailed);
     }
 
     @Override
     protected double getSpeed() {
-        return switch (type) {
-            case EUROPEAN -> getBaseSpeed();
-            case AFRICAN -> Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
-            case NORWEGIAN_BLUE -> (isNailed) ? 0 : getBaseSpeed(voltage);
-        };
+        return getBaseSpeed();
     }
 }
