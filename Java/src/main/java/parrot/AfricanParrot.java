04bfbb91823a71819f48d81b958b92bfd67f7911
sonjuwon <logicalstark1105@gmail.com>
2024-05-11 12:06:28 +0900

    R Remove redundant code in all subclasses

--- Java/src/main/java/parrot/AfricanParrot.java
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -1,16 +1,14 @@
 package parrot;
 
+import static parrot.ParrotTypeEnum.AFRICAN;
+
 public class AfricanParrot extends Parrot {
     protected AfricanParrot(final int numberOfCoconuts, final double voltage, final boolean isNailed) {
-        super(ParrotTypeEnum.AFRICAN, numberOfCoconuts, voltage, isNailed);
+        super(AFRICAN, numberOfCoconuts, voltage, isNailed);
     }
 
     @Override
     public double getSpeed() {
-        return switch (type) {
-            case EUROPEAN -> getBaseSpeed();
-            case AFRICAN -> Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
-            case NORWEGIAN_BLUE -> (isNailed) ? 0 : getBaseSpeed(voltage);
-        };
+        return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
     }
 }
